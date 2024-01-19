2c31108b3d009bc7a54bda7b6f0ac92c1d27088f
anasfalah3 <anasfalah3@gmail.com>
2024-01-19 22:02:30 +0100

    Header Component & Toggle NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-
+import Header from './components/header/Header';
 function App() {
   return (
-    <div className="App">
-      <h1> App</h1>
-    </div>
+    <>
+    <Header/>
+    </>
   );
 }
 
