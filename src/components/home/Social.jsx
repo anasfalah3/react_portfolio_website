b2f48330047483f8313d7c2de9b9f0f202a18013
anasfalah3 <anasfalah3@gmail.com>
2024-01-21 17:19:42 +0100

    Skills Component

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -7,7 +7,7 @@ const Social = () => {
         <i className="uil uil-instagram"></i>
       </a>
       <a href="https://www.linkedin.com/in/" className="home_social-icon" target="_blank">
-        <i class="uil uil-linkedin-alt"></i>
+        <i className="uil uil-linkedin-alt"></i>
       </a>
       <a href="https://github.com/" className="home_social-icon" target="_blank">
         <i className="uil uil-github-alt"></i>
---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+const Backend = () => {
+  return (
+      <div className="skills_content">
+      <h3 className="skills_title">Backend developer</h3>
+
+      <div className="skills_box">
+            <div className="skills_group">
+                  <div className="skills_data">
+                  <i className="bx bx-badge-check"></i>
+
+                  <div>
+                        <h3 className="skills_name">PHP</h3>
+                        <span className="skills_level">Intermediate</span>
+                  </div>
+                  </div>
+                  <div className="skills_data">
+                  <i className="bx bx-badge-check"></i>
+
+                  <div>
+                        <h3 className="skills_name">Laravel</h3>
+                        <span className="skills_level">Basic</span>
+                  </div>
+                  </div>
+                  <div className="skills_data">
+                  <i className="bx bx-badge-check"></i>
+
+                  <div>
+                        <h3 className="skills_name">Python</h3>
+                        <span className="skills_level">Intermediate</span>
+                  </div>
+                  </div>
+            </div>
+            <div className="skills_group">
+                  <div className="skills_data">
+                  <i className="bx bx-badge-check"></i>
+
+                  <div>
+                        <h3 className="skills_name">MySQL</h3>
+                        <span className="skills_level">Intermediate</span>
+                  </div>
+                  </div>
+                  <div className="skills_data">
+                  <i className="bx bx-badge-check"></i>
+
+                  <div>
+                        <h3 className="skills_name">MongoDB</h3>
+                        <span className="skills_level">Intermediate</span>
+                  </div>
+                  </div>
+                  <div className="skills_data">
+                  <i className="bx bx-badge-check"></i>
+
+                  <div>
+                        <h3 className="skills_name">SQL</h3>
+                        <span className="skills_level">Intermediate</span>
+                  </div>
+                  </div>
+            </div> 
+      </div>
+    </div>
+  )
+}
+
+export default Backend
