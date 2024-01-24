93d3bbb8d0bdec0338853e4a5ccf0f38bef13d59
anasfalah3 <anasfalah3@gmail.com>
2024-01-24 17:15:25 +0100

    Animated Cursor

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import './App.css';
-import Header from './components/header/Header'
-import Home from './components/home/Home'
-import About from './components/about/About';
-import Skills from './components/skills/Skills';
-import Services from './components/services/Services';
-import Qualification from './components/qualification/Qualification';
-import Work from './components/work/Work'
-import Testimonials from './components/testimonials/Testimonials';
-import Contact from './components/contact/Contact';
-import Footer from './components/footer/Footer';
-import ScrollUp from './components/scrollup/ScrollUp';
+import "./App.css";
+import AnimatedCursor from "react-animated-cursor";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
+import Services from "./components/services/Services";
+import Qualification from "./components/qualification/Qualification";
+import Work from "./components/work/Work";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import ScrollUp from "./components/scrollup/ScrollUp";
 function App() {
   return (
     <>
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={8}
+        color="000, 000, 000"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={5}
+      />
       <Header />
       <main className="main">
         <Home />
-        <About/>
-        <Skills/>
-        <Services/>
-        <Qualification/>
-        <Work/>
-        <Testimonials/>
-        <Contact/>
+        <About />
+        <Skills />
+        <Services />
+        <Qualification />
+        <Work />
+        <Testimonials />
+        <Contact />
       </main>
-        <Footer/>
-        <ScrollUp/>
+      <Footer />
+      <ScrollUp />
     </>
   );
 }
